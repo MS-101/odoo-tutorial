3afb71c0aeadc5513a2337ab1ed0e81c05d80562
Martin Šváb <martin.svab@gmail.com>
2025-04-23 11:44:05 +0200

    completed first 6 chapters of tutorial

---
+++ estate/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'estate',
+    'version': '1.0',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
+    ],
+    'author': 'Martin Šváb',
+    'description': 'Demo project for learning how to work in odoo framework.',
+    'application': True
+}
---
+++ estate/models/estate_property.py
@@ -0,0 +1,41 @@
+from datetime import timedelta
+from odoo import models, fields
+
+class EstateProperty(models.Model):
+    _name = 'estate_property'
+    _description = 'Real estate properties to be sold.'
+
+    name = fields.Char(string='Title', required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date(string='Available From',
+        copy=False, default=lambda self: fields.Date.today() + timedelta(days=90))
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer(string='Living Area (sqm)')
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer(string='Garden Area (sqm)')
+    garden_orientation = fields.Selection(
+        selection=[
+            ('north', 'North'),
+            ('south', 'South'),
+            ('east', 'East'),
+            ('west', 'West')
+        ]
+    )
+    state = fields.Selection(
+        required=True,
+        copy=False,
+        default='new',
+        selection=[
+            ('new', 'New'),
+            ('offer_received', 'Offer Received'),
+            ('offer_accepted', 'Offer Accepted'),
+            ('sold', 'Sold'),
+            ('cancelled', 'Cancelled')
+        ]
+    )
+    active = fields.Boolean(default=True)
